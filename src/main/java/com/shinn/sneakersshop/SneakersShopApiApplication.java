de1285f60ab51fe43d6787fc1351fec3c151f208
lamphamit <phamtanlambe@gmail.com>
2023-10-23 17:33:47 +0700

    initiate project

---
+++ src/main/java/com/shinn/sneakersshop/SneakersShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.shinn.sneakersshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakersShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakersShopApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shinn/sneakersshop/SneakersShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinn.sneakersshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakersShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
