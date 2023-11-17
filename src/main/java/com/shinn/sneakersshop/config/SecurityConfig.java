c0ad06219f530e649fe1c1cab9851260c03576e1
lamphamit <phamtanlambe@gmail.com>
2023-11-17 20:53:58 +0700

    add: fix cors issue

--- src/main/java/com/shinn/sneakersshop/config/SecurityConfig.java
+++ src/main/java/com/shinn/sneakersshop/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.shinn.sneakersshop.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,19 +10,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(requests ->
-                        requests
-                                .requestMatchers("/hello").authenticated()
-                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/register").permitAll()
-                )
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+        http.cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList("*"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setMaxAge(3600L);
+                return config;
+            }
+        })).csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(requests -> requests.requestMatchers("/hello").authenticated().requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/register").permitAll()).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
 
         return http.build();
 
