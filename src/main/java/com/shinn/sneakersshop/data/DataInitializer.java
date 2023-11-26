b3a9e7aaaae3f98e4d72dced1b2e5028e0c6148b
lamphamit <phamtanlambe@gmail.com>
2023-11-26 18:02:49 +0700

    add: init data

---
+++ src/main/java/com/shinn/sneakersshop/data/DataInitializer.java
@@ -0,0 +1,78 @@
+package com.shinn.sneakersshop.data;
+
+import com.shinn.sneakersshop.model.ERole;
+import com.shinn.sneakersshop.model.Role;
+import com.shinn.sneakersshop.model.Users;
+import com.shinn.sneakersshop.repository.RoleRepository;
+import com.shinn.sneakersshop.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+    private final RoleRepository roleRepository;
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public DataInitializer(RoleRepository roleRepository, UsersRepository usersRepository) {
+        this.roleRepository = roleRepository;
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role admin = new Role(
+                null,
+                new Date(System.currentTimeMillis()),
+                "Lam Pham",
+                null,
+                null,
+                "admin",
+                ERole.ADMIN,
+                null
+                );
+        Role user= new Role(
+                null,
+                new Date(System.currentTimeMillis()),
+                "Lam Pham",
+                null,
+                null,
+                "user",
+                ERole.USER,
+                null
+        );
+        Role seller = new Role(
+                null,
+                new Date(System.currentTimeMillis()),
+                "Lam Pham",
+                null,
+                null,
+                "seller",
+                ERole.SELLER,
+                null
+        );
+        roleRepository.save(admin);
+        roleRepository.save(user);
+        roleRepository.save(seller);
+        List<Role> roles = new ArrayList<>();
+        roles.add(user);
+        Users users = new Users(
+                null,
+                new Date(System.currentTimeMillis()),
+                "Lam Pham",
+                null,
+                null,
+                "lamphamit",
+                "lamdz2k4",
+                "phamtanlambe@gmail.com",
+                "Pham Tan Lam",
+                roles
+        );
+        usersRepository.save(users);
+    }
+}
