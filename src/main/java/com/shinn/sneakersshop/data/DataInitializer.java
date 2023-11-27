39fa2c9edb8295bfc6882861e28c7d52b167f0f7
lamphamit <phamtanlambe@gmail.com>
2023-11-27 14:47:02 +0700

    modify: encode intialize password

--- src/main/java/com/shinn/sneakersshop/data/DataInitializer.java
+++ src/main/java/com/shinn/sneakersshop/data/DataInitializer.java
@@ -1,78 +1,82 @@
-package com.shinn.sneakersshop.data;
-
-import com.shinn.sneakersshop.model.ERole;
-import com.shinn.sneakersshop.model.Role;
-import com.shinn.sneakersshop.model.Users;
-import com.shinn.sneakersshop.repository.RoleRepository;
-import com.shinn.sneakersshop.repository.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-@Component
-public class DataInitializer implements CommandLineRunner {
-    private final RoleRepository roleRepository;
-    private final UsersRepository usersRepository;
-
-    @Autowired
-    public DataInitializer(RoleRepository roleRepository, UsersRepository usersRepository) {
-        this.roleRepository = roleRepository;
-        this.usersRepository = usersRepository;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        Role admin = new Role(
-                null,
-                new Date(System.currentTimeMillis()),
-                "Lam Pham",
-                null,
-                null,
-                "admin",
-                ERole.ADMIN,
-                null
-                );
-        Role user= new Role(
-                null,
-                new Date(System.currentTimeMillis()),
-                "Lam Pham",
-                null,
-                null,
-                "user",
-                ERole.USER,
-                null
-        );
-        Role seller = new Role(
-                null,
-                new Date(System.currentTimeMillis()),
-                "Lam Pham",
-                null,
-                null,
-                "seller",
-                ERole.SELLER,
-                null
-        );
-        roleRepository.save(admin);
-        roleRepository.save(user);
-        roleRepository.save(seller);
-        List<Role> roles = new ArrayList<>();
-        roles.add(user);
-        Users users = new Users(
-                null,
-                new Date(System.currentTimeMillis()),
-                "Lam Pham",
-                null,
-                null,
-                "lamphamit",
-                "lamdz2k4",
-                "phamtanlambe@gmail.com",
-                "Pham Tan Lam",
-                roles
-        );
-        usersRepository.save(users);
-    }
-}
+//package com.shinn.sneakersshop.data;
+//
+//import com.shinn.sneakersshop.model.ERole;
+//import com.shinn.sneakersshop.model.Role;
+//import com.shinn.sneakersshop.model.Users;
+//import com.shinn.sneakersshop.repository.RoleRepository;
+//import com.shinn.sneakersshop.repository.UsersRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.ArrayList;
+//import java.util.Date;
+//import java.util.List;
+//
+//@Component
+//public class DataInitializer implements CommandLineRunner {
+//    private final RoleRepository roleRepository;
+//    private final UsersRepository usersRepository;
+//
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    public DataInitializer(RoleRepository roleRepository, UsersRepository usersRepository, PasswordEncoder passwordEncoder) {
+//        this.roleRepository = roleRepository;
+//        this.usersRepository = usersRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        Role admin = new Role(
+//                null,
+//                new Date(System.currentTimeMillis()),
+//                "Lam Pham",
+//                null,
+//                null,
+//                "admin",
+//                ERole.ADMIN,
+//                null
+//                );
+//        Role user= new Role(
+//                null,
+//                new Date(System.currentTimeMillis()),
+//                "Lam Pham",
+//                null,
+//                null,
+//                "user",
+//                ERole.USER,
+//                null
+//        );
+//        Role seller = new Role(
+//                null,
+//                new Date(System.currentTimeMillis()),
+//                "Lam Pham",
+//                null,
+//                null,
+//                "seller",
+//                ERole.SELLER,
+//                null
+//        );
+//        roleRepository.save(admin);
+//        roleRepository.save(user);
+//        roleRepository.save(seller);
+//        List<Role> roles = new ArrayList<>();
+//        roles.add(user);
+//        Users users = new Users(
+//                null,
+//                new Date(System.currentTimeMillis()),
+//                "Lam Pham",
+//                null,
+//                null,
+//                "lamphamit",
+//                passwordEncoder.encode("lamdz2k4"),
+//                "phamtanlambe@gmail.com",
+//                "Pham Tan Lam",
+//                roles
+//        );
+//        usersRepository.save(users);
+//    }
+//}
--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-# INSERT INTO role(code, name) VALUES ('admin', 'ADMIN'), ('seller', 'SELLER'), ('user', 'USER')
-#
-# INSERT INTO user(username, password, fullname, actived_email) VALUES('lamphamit', 'lamdz2k4', 'Pham Tan Lam', 'phamtanlambe@gmail.com'),
-#                                                                     ('admin', 'lamdz2k4', 'Ricky', 'ricky@gmail.com'),
-#                                                                     ('seller', 'lamdz2k4', 'Fatt', 'fatt@gmail.com')
-#
-# INSERT INTO user_roles(user_id, role_id) VALUES(1, 3),
-#                                             (2, 1),
-#                                             (3, 2)
